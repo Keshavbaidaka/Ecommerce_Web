49a688453a5fdea6a2341b3cf5a7b3b08789bb7a
Keshav Baidaka <162796642+Keshavbaidaka@users.noreply.github.com>
2025-07-29 20:59:45 +0530

    Update Login.jsx

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("/api/v1/auth/login", {
+      const res = await axios.post("https://ecommerce-web-backend-g4hs.onrender.com/api/v1/auth/login", {
         email,
         password,
       });
