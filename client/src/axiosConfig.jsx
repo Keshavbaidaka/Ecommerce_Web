c75042a39110369108b73de7babd8de9a7a754cb
Keshav Baidaka <162796642+Keshavbaidaka@users.noreply.github.com>
2025-07-29 21:14:57 +0530

    Update axiosConfig.jsx

--- client/src/axiosConfig.jsx
+++ client/src/axiosConfig.jsx
@@ -1,1 +1,10 @@
 
+// src/axiosConfig.js
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: false, // change to true if using cookies
+});
+
+export default instance;
