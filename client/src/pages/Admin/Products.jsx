c8878fa641370efd891972048ca4ea3376382c69
Keshavbaidaka <keshavbaidaka@gmail.com>
2025-07-29 22:57:28 +0530

    adding img link

--- client/src/pages/Admin/Products.jsx
+++ client/src/pages/Admin/Products.jsx
@@ -39,7 +39,7 @@ const Products = () => {
               >
                 <div className="card m-2" style={{ width: "18rem" }}>
                   <img
-                    src={`/api/v1/product/product-photo/${p._id}`}
+                    src={`https://ecommerce-web-backend-g4hs.onrender.com/api/v1/product/product-photo/${p._id}`}
                     className="card-img-top"
                     alt={p.name}
                   />
